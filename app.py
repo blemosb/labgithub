b176cd3ffc9b7c01153727d8b7edeec178edb00c
blemosb <blemosb@gmail.com>
2024-06-10 20:22:02 -0300

    Revert "uma inclusao no menu"
    This reverts commit 7b53bf36ed7f56c4249996803441a1ea30f0990a.

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def main():
         print("1. Mostrar tarefas")
         print("2. Adicionar tarefa")
         print("3. Sair")
-        print("4. TESTE")
         choice = input("Escolha uma opção: ")
 
         if choice == '1':
